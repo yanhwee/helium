4c4edff1cb0e75a448824446ec765d9b9cff11aa
Yan Hwee <yanhweeyee@gmail.com>
2020-05-11 22:02:55 +0800

    Lidar Support

--- src/helium/setup.bash
+++ src/helium/setup.bash
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:$DIR/models
+export GAZEBO_MODEL_PATH=$DIR/models:${GAZEBO_MODEL_PATH}
+
+export GAZEBO_RESOURCE_PATH=$DIR/worlds:${GAZEBO_RESOURCE_PATH}
